2ff4ef94d6ae876d42dbf08e5ba6d0019d5d2b2a
wysohn <wysohn2012@gmail.com>
2023-02-20 01:02:57 -0800

    accept login provider

--- src/start.bat
+++ src/start.bat
@@ -5,6 +5,10 @@ CALL node -v >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 GOTO NODENOTFOUND
 
 ::Accept username and password from command arguemnts
+:LOGIN_TYPE
+set /p login_type="Login type? (i=instagram, f=facebook)>> "
+if "%login_type%"=="" goto LOGIN_TYPE
+
 :INPUT_USERNAME
 set /p user="User Name?>> "
 if "%user%"=="" goto INPUT_USERNAME
@@ -16,8 +20,6 @@ if "%pass%"=="" goto INPUT_PASSWORD
 :INPUT_GUI
 set /p gui="GUI? (y/n)>> "
 if "%gui%"=="" goto INPUT_GUI
-
-cls
 
 ECHO User name: %user%
 GOTO APP
@@ -32,14 +34,18 @@ call npm ci
 setlocal 
     if "%gui%"=="y" (set GUI="true") else (set GUI="false")
 
+    if "%login_type%"=="i" (set loginType=instagram) 
+    if "%login_type%"=="f" (set loginType=facebook) else (set loginType=instagram)
+
     SETLOCAL EnableDelayedExpansion
     set USER_ID=!user!
     set PASSWORD=!pass!
-    start node index.js !GUI!
+    set LOGIN_PROVIDER=!loginType!
+
+    call node index.js !GUI!
 endlocal
 
-ECHO Closing in 5 seconds...
-timeout 5 > NUL
+pause
 
 EXIT 0
 
